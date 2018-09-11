48d9c8b8f81cc44a71e6e73a19de085cdcb493e5
LuiCat <zhtl1995@163.com>
2018-09-11 17:38:29 +0200

    Created MIDI importer

---
+++ Assets/Scripts/MIDI/MIDIChart.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MIDIChart : ScriptableObject
+{
+    public class Note : ScriptableObject
+    {
+        public int noteNum;
+        public float beginBeat;
+        public float endBeat;
+    }
+
+    public class Track : ScriptableObject
+    {
+        public Note[] notes;
+    }
+
+    public Track[] tracks;
+
+}
---
+++ Assets/Scripts/MIDI/MIDIImporter.cs
@@ -0,0 +1,51 @@
+﻿using Melanchall.DryWetMidi.Smf;
+using Melanchall.DryWetMidi.Smf.Interaction;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.AssetImporters;
+using UnityEngine;
+
+[ScriptedImporter(1, new string[] { "mid", "midi" })]
+public class MIDIImporter : ScriptedImporter
+{
+
+    public override void OnImportAsset(AssetImportContext ctx)
+    {
+        MIDIChart chart = ScriptableObject.CreateInstance<MIDIChart>();
+
+        MidiFile midi = MidiFile.Read(ctx.assetPath);
+
+        float beatTicks = ((TicksPerQuarterNoteTimeDivision)midi.TimeDivision).TicksPerQuarterNote;
+
+        var trackList = new List<MIDIChart.Track>();
+
+        foreach (var chunk in midi.Chunks)
+        {
+            var track = chunk as TrackChunk;
+            if (track == null)
+                continue;
+
+            var noteList = new List<MIDIChart.Note>();
+
+            var notesManager = new NotesManager(track.Events);
+            foreach (var midiNote in notesManager.Notes)
+            {
+                var note = ScriptableObject.CreateInstance<MIDIChart.Note>();
+                note.noteNum = midiNote.NoteNumber;
+                note.beginBeat = midiNote.Time / beatTicks;
+                note.endBeat = midiNote.Length / beatTicks;
+                noteList.Add(note);
+            }
+
+            var trackData = ScriptableObject.CreateInstance<MIDIChart.Track>();
+            trackData.notes = noteList.ToArray();
+            trackList.Add(trackData);
+        }
+
+        chart.tracks = trackList.ToArray();
+
+        ctx.AddObjectToAsset("chart", chart);
+        ctx.SetMainObject(chart);
+    }
+
+}
