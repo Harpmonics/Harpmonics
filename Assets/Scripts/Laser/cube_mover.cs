c139ca5eae2a1c026403137cf541aea2eff3cb98
Jacob Tärning <jacob_tarning@n155-p75.eduroam.kth.se>
2018-09-14 16:50:44 +0200

    added local laser prefab to project

---
+++ Assets/Scripts/Laser/cube_mover.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_movement : MonoBehaviour
+{
+
+	
+	void Update ()
+	{
+		if(Input.GetKey("right"))
+		{
+			transform.position = new Vector3(transform.position.x + 0.5f, transform.position.y, transform.position.z);
+		}
+		else if(Input.GetKey("left"))
+		{
+			transform.position = new Vector3(transform.position.x - 0.5f, transform.position.y, transform.position.z);
+		}
+		else if(Input.GetKey("up"))
+		{
+			transform.position = new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z);
+		}
+		else if(Input.GetKey("down"))
+		{
+			transform.position = new Vector3(transform.position.x, transform.position.y - 0.5f, transform.position.z);
+		}
+	}
+}
