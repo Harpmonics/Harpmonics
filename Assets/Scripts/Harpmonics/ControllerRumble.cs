e6c41306d1ed9f0848c21fe77edc7e373bc7458d
Anton <antthu@kth.se>
2018-09-17 19:42:25 +0200

    Updated properties to match actual functionality

--- Assets/Scripts/Harpmonics/ControllerRumble.cs
+++ Assets/Scripts/Harpmonics/ControllerRumble.cs
@@ -8,10 +8,10 @@ public class ControllerRumble : ATouchCallee
 {
     [Tooltip("Intensity of the rumble."), Range(0, 1)]
     public float rumbleStrength = 0.1f;
-    [Tooltip("How long to rumble for."), Range(0, 5)]
-    public float rumbleDuration = 0.1f;
-    [Tooltip("Interval to wait before triggering next rumble in succession."), Range(0, 10)]
-    public float rumbleInterval = 0.1f;
+
+    // Magic variable taken from https://steamcommunity.com/app/358720/discussions/0/405693392914144440/#c357284767229628161
+    // Max possible rumble duration
+    private static int MAX_DURATION = 3999;
 
     protected bool isRumbling = false;
 
@@ -29,7 +29,8 @@ public class ControllerRumble : ATouchCallee
 
     public void Update()
     {
+        // Since VRTK TriggerHapticPulse does not seem to work, we call the SteamVR interface directly
         foreach (GameObject activator in rumblingControllers)
-            SteamVR_Controller.Input((int)activator.GetComponent<SteamVR_RenderModel>().index).TriggerHapticPulse(500);
+            SteamVR_Controller.Input((int)activator.GetComponent<SteamVR_RenderModel>().index).TriggerHapticPulse((ushort)(rumbleStrength * MAX_DURATION));
     }
 }
