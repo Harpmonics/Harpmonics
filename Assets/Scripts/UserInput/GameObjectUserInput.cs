5f7f7c3a80aae732938d776c02f0c62fbace0793
Mikael Forsberg <miforsb@kth.se>
2018-09-17 18:23:09 +0200

    alternative laserstage_vr using the IUserInputSource idea

---
+++ Assets/Scripts/UserInput/GameObjectUserInput.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A user input mapper that simply reports the positions of two given GameObjects
+/// </summary>
+public class GameObjectUserInput : MonoBehaviour, IUserInputSource
+{
+    public GameObject leftHand;
+    public GameObject rightHand;
+
+    /// <returns>
+    /// True if the input source is currently providing position data for the left "touch" trigger, false otherwise
+    /// </returns>
+    public bool isProvidingLeftTouch()
+    {
+        return true;
+    }
+
+    /// <returns>
+    /// World position of left "touch" trigger. If the input source is not providing this data, returns the origin (0,0,0)
+    /// </returns>
+    public Vector3 getLeftTouchWorldPosition()
+    {
+        return leftHand.transform.position;
+    }
+
+    /// <returns>
+    /// True if the input source is currently providing position data for the right "touch" trigger, false otherwise
+    /// </returns>
+    public bool isProvidingRightTouch()
+    {
+        return true;
+    }
+
+    /// <returns>
+    /// World position of right "touch" trigger. If the input source is not providing this data, returns the origin (0,0,0)
+    /// </returns>
+    public Vector3 getRightTouchWorldPosition()
+    {
+        return rightHand.transform.position;
+    }
+}
---
+++ Assets/SynthControllerNotifier.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SynthControllerNotifier : MonoBehaviour
+{
+    public string triggerTag;
+    public GameObject synth;
+    public int track;
+    private SynthesizerControl_alt synthControl;
+
+	// Use this for initialization
+	void Start ()
+    {
+        synthControl = synth.GetComponent<SynthesizerControl_alt>();
+	}
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (synthControl != null && other.CompareTag(triggerTag))
+        {
+            synthControl.JudgeTrack(track);
+        }
+    }
+}
