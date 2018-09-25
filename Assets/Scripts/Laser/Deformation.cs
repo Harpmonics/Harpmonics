7b778a6e7407efc5d946680b56aabe42a6356669
Jacob Tärning <jacob_tarning@n152-p29.eduroam.kth.se>
2018-09-25 17:29:07 +0200

    Deformtion scripts and multivertex cylinder

---
+++ Assets/Scripts/Laser/Deformation.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using Array;
+
+[RequireComponent(typeof(MeshFilter))]
+public class Deformation : MonoBehaviour
+{
+	Mesh mesh;
+	Vector3[] ogVert, disVert, vertexVelocities;
+    public float springForce = 20f;
+    float uniformScale = 1f;
+    public float damping = 5f;
+
+    void Start ()
+	{
+		mesh = GetComponent<MeshFilter>().mesh;
+		ogVert = mesh.vertices;
+		disVert = new Vector3[ogVert.Length];
+		for (int i = 0; i < ogVert.Length; i++)
+		{
+			disVert[i] = ogVert[i];
+		}
+        vertexVelocities = new Vector3[ogVert.Length];
+
+        //VerticeLocations(ogVert);
+    }
+	
+	void Update ()
+	{
+        uniformScale = transform.localScale.x;
+        for (int i = 0; i < disVert.Length; i++)
+        {
+            UpdateVertex(i);
+        }
+        mesh.vertices = disVert;
+        mesh.RecalculateNormals();
+    }
+
+	public void AddDeformForce (Vector3 point, float force)
+	{
+        Debug.DrawLine(Camera.main.transform.position, point);
+        point = transform.InverseTransformPoint(point);
+        for (int i = 0; i < disVert.Length; i++)
+        {
+            AddForceToVertex(i, point, force);
+        }
+    }
+
+    void AddForceToVertex(int i, Vector3 point, float force)
+    {
+        Vector3 pointToVertex = disVert[i] - point;
+        pointToVertex *= uniformScale;
+        float attenuatedForce = force / (1f + pointToVertex.sqrMagnitude);
+        float velocity = attenuatedForce * Time.deltaTime;
+        vertexVelocities[i] += pointToVertex.normalized * velocity;
+    }
+
+    void UpdateVertex (int i)
+    {
+        Vector3 velocity = vertexVelocities[i];
+        Vector3 displacement = disVert[i] - ogVert[i];
+        displacement *= uniformScale;
+        velocity -= displacement * springForce * Time.deltaTime;
+        velocity *= 1f - damping * Time.deltaTime; // remove this line for eternally oscillating strings
+        vertexVelocities[i] = velocity;
+        disVert[i] += velocity * (Time.deltaTime / uniformScale);
+    }
+
+    // print location of every vertice
+    private void VerticeLocations (Vector3[] vertices)
+    {
+        for (int i = 0; i < vertices.Length; i++)
+        { Debug.Log(vertices[i]); }
+    }
+}
---
+++ Assets/Scripts/Laser/Deformation_input.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deformation_input : MonoBehaviour
+{
+	public float force = 10f;
+    public float offset = 0.1f;
+	
+	void Update ()
+	{
+        if (Input.GetMouseButton(0))
+        {
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+
+			if (Physics.Raycast(ray, out hit))
+			{
+                // this below is the important part
+				Deformation deform = hit.collider.GetComponent<Deformation>();
+				if (deform)
+				{
+					Vector3 point = hit.point;
+                    point += hit.normal * offset;
+					deform.AddDeformForce(point, force);
+				}
+			}
+		}
+	}
+}
