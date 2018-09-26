8cd865fb76b88e36de51f0fbebad245cee07e254
Anton <antthu@kth.se>
2018-09-26 15:05:49 +0200

    Added menu & tooltips

---
+++ Assets/Scripts/Menu/LoadScene.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+using VRTK;
+
+public class LoadScene : MonoBehaviour
+{
+    [Tooltip("Menu scene to be activated when user wants to exit to menu.")]
+    public string menuScene = "Menu";
+
+    public void Start()
+    {
+        InputManager.AddCallback(OnControllerCallback);
+
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene arg0, LoadSceneMode arg1)
+    {
+        VRTK_SDKManager.AttemptTryLoadSDKSetupFromList(false);
+    }
+
+    private void OnControllerCallback(InputManager.InputState inputState)
+    {
+        if (inputState.Buttons.HasFlag(InputManager.InputState.ActiveFunction.OpenMenu))
+            Load(menuScene);
+    }
+
+    public void Load(string scene)
+    {
+        // SceneManager.LoadScene(scene, LoadSceneMode.Single);
+
+        // SteamVR has asynchronous loading, but we could use the above
+        SteamVR_LoadLevel.Begin(scene, true, 0.5f, 0, 0, 0, 1);
+    }
+}
---
+++ Assets/Scripts/Menu/TooltipManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class TooltipManager : MonoBehaviour
+{
+
+    private VRTK_ControllerTooltips tooltips;
+
+    void Start()
+    {
+        InputManager.AddCallback(OnControllerCallback);
+
+        tooltips = GetComponent<VRTK_ControllerTooltips>();
+
+        tooltips.UpdateText(VRTK_ControllerTooltips.TooltipButtons.ButtonTwoTooltip, "Record a motion sequence");
+    }
+
+    private void OnControllerCallback(InputManager.InputState inputState)
+    {
+        if (inputState.Buttons.HasFlag(InputManager.InputState.ActiveFunction.SequenceRecord))
+        {
+            tooltips.UpdateText(VRTK_ControllerTooltips.TooltipButtons.ButtonTwoTooltip, "");
+            tooltips.UpdateText(VRTK_ControllerTooltips.TooltipButtons.TouchpadTooltip, "Save sequence into top or bottom slot");
+        }
+
+        if (tooltips.buttonTwoText.Length == 0 && (inputState.Buttons & InputManager.InputState.ActiveFunction.SequencePlaybacks) > 0)
+        {
+            tooltips.UpdateText(VRTK_ControllerTooltips.TooltipButtons.ButtonTwoTooltip, "Record a motion sequence");
+            tooltips.UpdateText(VRTK_ControllerTooltips.TooltipButtons.TouchpadTooltip, "Replay sequence saved into top or bottom slot");
+        }
+    }
+}
