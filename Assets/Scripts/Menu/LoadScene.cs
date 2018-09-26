cfc0b21ce2b84f51784439017f5c2999c76e8e2f
Anton <antthu@kth.se>
2018-09-26 20:23:59 +0200

    Enabled customization for scene loading screen

--- Assets/Scripts/Menu/LoadScene.cs
+++ Assets/Scripts/Menu/LoadScene.cs
@@ -11,15 +11,22 @@ public class LoadScene : MonoBehaviour
     [Tooltip("Menu scene to be activated when user wants to exit to menu.")]
     public string menuScene = "Menu";
 
+    private SteamVR_LoadLevel loadLevelScript;
+
     public void Start()
     {
         InputManager.AddCallback(OnControllerCallback);
 
         SceneManager.sceneLoaded += OnSceneLoaded;
+
+        loadLevelScript = GetComponent<SteamVR_LoadLevel>();
+            
+        loadLevelScript.levelName = menuScene;
     }
 
     private void OnSceneLoaded(Scene arg0, LoadSceneMode arg1)
     {
+        // TODO: Check if this is still necessary
         VRTK_SDKManager.AttemptTryLoadSDKSetupFromList(false);
     }
 
@@ -31,9 +38,6 @@ public class LoadScene : MonoBehaviour
 
     public void Load(string scene)
     {
-        // SceneManager.LoadScene(scene, LoadSceneMode.Single);
-
-        // SteamVR has asynchronous loading, but we could use the above
-        SteamVR_LoadLevel.Begin(scene, true, 0.5f, 0, 0, 0, 1);
+        loadLevelScript.Trigger();
     }
 }
