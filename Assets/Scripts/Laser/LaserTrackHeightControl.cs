601d39782606a437e961f32600103471d4bb0c77
Anton <antthu@kth.se>
2018-09-27 20:52:39 +0200

    Changed height tracking to use VRTK references

--- Assets/Scripts/Laser/LaserTrackHeightControl.cs
+++ Assets/Scripts/Laser/LaserTrackHeightControl.cs
@@ -2,17 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LaserTrackHeightControl : MonoBehaviour {
-    
-    public Transform leftHandObject;
-    public Transform rightHandObject;
-    public Transform headsetObject;
+using VRTK;
 
+public class LaserTrackHeightControl : MonoBehaviour
+{
     public float headsetFreezeYThreshold = 0.1f;
     public float headsetFreezeTime = 1f;
     public float offsetFromHeadset = 0.5f;
     public float trackFollowDamping = 0.8f;
 
+    private Transform leftHandObject;
+    private Transform rightHandObject;
+    private Transform headsetObject;
+    
     Vector3 lastLeftHandPosition;
     Vector3 lastRightHandPosition;
     Vector3 lastHeadsetPosition;
@@ -20,11 +22,31 @@ public class LaserTrackHeightControl : MonoBehaviour {
     float timeHeadsetMoved = 0;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
+        VRTK_SDKManager.SubscribeLoadedSetupChanged(SDK_Loaded);
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    private void SDK_Loaded(VRTK_SDKManager sender, VRTK_SDKManager.LoadedSetupChangeEventArgs e)
+    {
+        VRTK_SDKSetup setup = e.currentSetup;
+
+        // When switching scenes, we can get a non-existent SDK for some reason
+        if (setup == null)
+            return;
+
+        leftHandObject = setup.actualLeftController.transform;
+        rightHandObject = setup.actualRightController.transform;
+
+        headsetObject = setup.actualHeadset.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (leftHandObject == null)
+            return;
+
         Vector3 leftHandPosition = leftHandObject.position;
         Vector3 rightHandPosition = rightHandObject.position;
         Vector3 headsetPosition = headsetObject.position;
