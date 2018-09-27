2dfba6dd405d7d2a11236e18ddc045e3b0e7fd5c
Jacob Tärning <jacob_tarning@n181-p133.eduroam.kth.se>
2018-09-27 14:26:30 +0200

    changes to test on vive

---
+++ Assets/Scripts/Laser/Deformation_controller_input.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deformation_controller_input : ATouchCallee
+{
+
+    public float force = 10f;
+    public float offset = 0.1f;
+
+    void Update ()
+    {
+		
+	}
+
+    public override void Callback(GameObject caller, GameObject activator, bool touching)
+    {
+        if (touching)
+        {
+            Deformation deform = caller.GetComponent<Deformation>();
+
+            if (deform)
+            {
+                Vector3 point = caller.GetComponent<Collider>().ClosestPoint(activator.transform.position);
+                //point += hit.normal * offset;
+                // need to extract the normal of the laser surface
+                // might be a bit of a hack, create a raycast from the positions of the caller and activator and extract the normal from it
+                Ray ray = new Ray(activator.transform.position, caller.transform.position);
+                RaycastHit hit;
+                Physics.Raycast(ray, out hit);
+
+                point += hit.normal * offset;
+                deform.AddDeformForce(point, force);
+            }
+        }
+    }
+}
