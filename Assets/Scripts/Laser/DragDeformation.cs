50e3d046791cce82676a8265faf09c18c0c016c8
Jacob Tärning <jacob_tarning@Jacobs-MBP.lan>
2018-10-24 23:25:46 +0200

    initial elasticity

---
+++ Assets/Scripts/Laser/DragDeformation.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshCollider))]
+public class DragDeformation : MonoBehaviour 
+{
+
+    Mesh mesh, colliderMesh;
+    /*
+    Original Vertices
+    Displaced Vertices
+    Original Collider Vertices
+    Displaced Collider Vertices
+    Distance to Center
+    */
+    Vector3[] ov, dv, ocv, dcv, d2c;
+
+    void Start ()
+    {
+        mesh = GetComponent<MeshFilter>().mesh;
+        ov = mesh.vertices;
+        dv = new Vector3[ov.Length];
+        d2c = new Vector3[ov.Length];
+
+        colliderMesh = GetComponent<MeshCollider>().sharedMesh;
+        ocv = colliderMesh.vertices;
+        dcv = new Vector3[ocv.Length];
+
+        for (int i = 0; i < ov.Length; i++)
+        {
+            dv[i] = ov[i];
+            dcv[i] = ocv[i];
+
+            d2c[i] = ov[i] - transform.InverseTransformPoint(transform.position);
+        }
+    }
+
+    void Update ()
+    {
+        mesh.vertices = dv;
+        colliderMesh.vertices = dcv;
+        mesh.RecalculateNormals();
+        mesh.RecalculateBounds();
+        colliderMesh.RecalculateNormals();
+        colliderMesh.RecalculateBounds();
+    }
+
+    public void Deform(Vector3 point)
+    {
+        point = transform.InverseTransformPoint(point);
+        for (int i = 0; i < dv.Length; i++)
+        {
+            DisplaceVertex(i, point);
+        }
+    }
+
+    private void DisplaceVertex(int i, Vector3 point)
+    {
+        // scale the distance moved depending on how far from contact point the vertex is
+        Vector3 dist = point - dv[i];
+        // the distance factor is usually very small
+        float df = dist.sqrMagnitude * 1000f;
+        // update vertex positions of both meshes
+        dv[i] += (dist + d2c[i]) * (1f / (1f + df));
+        dcv[i] = dv[i];
+    }
+}
---
+++ Assets/Scripts/Laser/DragDeformationInput.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(DragDeformation))]
+public class DragDeformationInput : MonoBehaviour
+{
+
+    DragDeformation deform;
+    Vector3 point, oldPoint;
+    GameObject dragger;
+    bool grabbed = false;
+
+	void Start ()
+    {
+        deform = GetComponent<DragDeformation>();
+        oldPoint = new Vector3(0f,0f,0f);
+	}
+
+    void Update ()
+    {
+        if (grabbed)
+        {
+            if (!(point == oldPoint))
+            {
+                deform.Deform(point);
+                oldPoint = point;
+            }
+            point = dragger.transform.position;
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (InputManager.IsUserInput(other))
+        {
+            if (deform)
+            {
+                point = GetComponent<Collider>().ClosestPoint(other.transform.position);
+                dragger = other.gameObject;
+                grabbed = true;
+            }
+        }
+    }
+}
