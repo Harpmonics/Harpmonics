ca26967c564f7d600cc3791344783ba086b6f0ec
Anton <antthu@kth.se>
2018-10-30 21:48:17 +0100

    Added rumble on note hit

--- Assets/Scripts/Controller/ControllerRumble.cs
+++ Assets/Scripts/Controller/ControllerRumble.cs
@@ -4,33 +4,47 @@ using UnityEngine;
 
 using VRTK;
 
-public class ControllerRumble : ATouchCallee
+public class ControllerRumble : MonoBehaviour
 {
     [Tooltip("Intensity of the rumble."), Range(0, 1)]
     public float rumbleStrength = 0.1f;
+
+    [Tooltip("Rumble time in seconds.")]
+    public float rumbleTime = 0.1f;
 
     // Magic variable taken from https://steamcommunity.com/app/358720/discussions/0/405693392914144440/#c357284767229628161
     // Max possible rumble duration
     private static int MAX_DURATION = 3999;
 
-    protected bool isRumbling = false;
-
-    protected List<GameObject> rumblingControllers = new List<GameObject>();
-
-    public override void Callback(GameObject caller, GameObject activator, bool touching)
+    public void StartRumble(Collider controllerCollider, float rumbleTime = -1f)
     {
-        isRumbling = touching;
+        if (!InputManager.IsUserInput(controllerCollider))
+            return;
 
-        if (touching)
-            rumblingControllers.Add(activator);
-        else
-            rumblingControllers.Remove(activator);
+        if (rumbleTime < 0)
+            rumbleTime = this.rumbleTime;
+
+        StartCoroutine(RumbleCoroutine(controllerCollider.gameObject, rumbleTime));
     }
 
-    public void Update()
+    IEnumerator RumbleCoroutine(GameObject controller, float rumbleTime)
     {
-        // Since VRTK TriggerHapticPulse does not seem to work, we call the SteamVR interface directly
-        foreach (GameObject activator in rumblingControllers)
-            SteamVR_Controller.Input((int)activator.GetComponent<SteamVR_RenderModel>().index).TriggerHapticPulse((ushort)(rumbleStrength * MAX_DURATION));
+        float remainingTime = rumbleTime;
+
+        SteamVR_RenderModel model = GetComponent<SteamVR_RenderModel>();
+
+        // Simulator, can't rumble
+        if (model == null)
+            yield break;
+
+        while (remainingTime > 0)
+        {
+            // Since VRTK TriggerHapticPulse does not seem to work, we call the SteamVR interface directly
+            SteamVR_Controller.Input((int)model.index).TriggerHapticPulse((ushort)(rumbleStrength * MAX_DURATION));
+
+            remainingTime -= Time.deltaTime;
+
+            yield return null;
+        }
     }
 }
