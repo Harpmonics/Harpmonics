03fd931d301d2186ecaea808976b6bdb26d77097
VIC <>
2018-10-31 16:26:43 +0100

    Changed timing/scoring values

--- Assets/Scripts/Laser/LaserMIDITimingJudge.cs
+++ Assets/Scripts/Laser/LaserMIDITimingJudge.cs
@@ -10,52 +10,57 @@ public class LaserMIDITimingJudge : MonoBehaviour {
     
     int nextJudgedNote = 0;
 
-    public float toleranceBeat = 0.25f;
+    public float toleranceBeatOK = 0.5f;
+    public float toleranceBeatGood = 0.25f;
+    public float toleranceBeatPerfect = 0.125f;
+
+    public int scoreOK = 20;
+    public int scoreGood = 50;
+    public int scorePerfect = 100;
 
     private bool alreadyInitialized = false;
 
     public MIDIChart.Note HitNoteOnBeat(float beat)
     {
-        var tmpNote = new MIDIChart.Note { noteNum = -1, beginBeat = beat - toleranceBeat };
+        var tmpNote = new MIDIChart.Note { noteNum = -1, beginBeat = beat - toleranceBeatOK };
         int index = Array.BinarySearch(notes, tmpNote, Comparer<MIDIChart.Note>.Create((note1, note2) => note1.beginBeat.CompareTo(note2.beginBeat)));
         if (index < 0) index = ~index;
         while (index + 1 < notes.Length && Mathf.Abs(notes[index + 1].beginBeat - beat) <= Mathf.Abs(notes[index].beginBeat - beat)) ++index;
-		if (index < notes.Length && index - 1 != nextJudgedNote && Mathf.Abs(notes[index].beginBeat - beat) <= toleranceBeat && !notes[index].played)
+		if (index < notes.Length && index - 1 != nextJudgedNote && Mathf.Abs(notes[index].beginBeat - beat) <= toleranceBeatOK && !notes[index].played)
         {
-            float accuracy = 1 - Mathf.Abs(notes[index].beginBeat - beat) / toleranceBeat;
+            float diffBeat = Mathf.Abs(notes[index].beginBeat - beat);
+            float accuracy = 1 - diffBeat / toleranceBeatOK;
 
             AccuracyGraph.TrackAccuracy(this.gameObject, accuracy, index);
-
-            if (Mathf.Abs(notes[index].beginBeat - beat) <= 0.10)
+            
+            if (diffBeat <= toleranceBeatPerfect)
 			{
 				Feedback.alpha = 1f;
-				ScoreStat.Score += 100;
+				ScoreStat.Score += scorePerfect;
 				//print(Mathf.Abs(notes[index].beginBeat - beat) + " should reward 100 points");
 				Feedback.fb = "Perfect";
 			}
-			
-			else if (Mathf.Abs(notes[index].beginBeat - beat) <= 0.17 && Mathf.Abs(notes[index].beginBeat - beat) > 0.10)
+			else if (diffBeat <= toleranceBeatGood)
 			{
 				Feedback.alpha = 1f;
-				ScoreStat.Score += 70;
-				//print(Mathf.Abs(notes[index].beginBeat - beat) + " should reward 70 points");
+				ScoreStat.Score += scoreGood;
+				//print(Mathf.Abs(notes[index].beginBeat - beat) + " should reward 50 points");
 				Feedback.fb = "Good";
-
 			}
-			
 			else
 			{
 				Feedback.alpha = 1f;
-				ScoreStat.Score += 50;
-				//print(Mathf.Abs(notes[index].beginBeat - beat) + " should reward 50 points");
+				ScoreStat.Score += scoreOK;
+				//print(Mathf.Abs(notes[index].beginBeat - beat) + " should reward 20 points");
 				Feedback.fb = "Ok";
 			}
-			
-			notes[index].played = true;
+
+            notes[index].played = true;
 			nextJudgedNote = index + 1;
 			return notes[index];
-			
+
         }
+
         return null;
     }
 
@@ -87,7 +92,7 @@ public class LaserMIDITimingJudge : MonoBehaviour {
     {
         get
         {
-            return notes != null && nextJudgedNote < notes.Length && notes[nextJudgedNote].beginBeat < BeatTime.beat + toleranceBeat ? notes[nextJudgedNote] : null;
+            return notes != null && nextJudgedNote < notes.Length && notes[nextJudgedNote].beginBeat < BeatTime.beat + toleranceBeatOK ? notes[nextJudgedNote] : null;
         }
     }
 
